2a5597e8af4ddb626258c5fbcaa9e1cabca76e9e
mateusz <mateuszalmannai@googlemail.com>
2014-12-04 15:04:32 +0100

    add more files

--- dot.sh
+++ dot.sh
@@ -5,8 +5,9 @@
 # to any desired dotfiles in ~/dotfiles
 #################################################
 
-declare -r dir=~/dotfiles
-declare -r old_dir=~/dotfiles_old
+declare -r dir=~/.dotfiles
+declare -r old_dir=~/.dotfiles_old
+declare -r time_stamp=$(date +%Y-%m-%d-%H-%M-%S)
 declare -r files="bashrc vimrc vim zshrc oh-my-zsh"
 
 echo "Creating $old_dir for backup of any existing dotfiles in ~"
@@ -20,7 +21,7 @@ echo "...done"
 # then create symlinks
 for file in $files; do 
   echo "Moving any existing dotfiles from ~ to $old_dir"
-  mv ~/.$file ~/dotfiles_old/
+  mv ~/.$file ~/dotfiles_old/$file_$time_stamp
   echo "Creating symlink to $file in home directory."
   ln -s $dir/$file ~/.$file
 done
